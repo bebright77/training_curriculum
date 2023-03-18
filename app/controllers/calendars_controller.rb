2aafa15a2068ba29ee05693b126e6ce900d32b9d
bebright77 <aki.tomita.photo@gmail.com>
2023-03-18 21:03:32 +0900

    issue6再提出

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -23,7 +23,7 @@ class CalendarsController < ApplicationController
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
-    @todays_wday = Date.today.wday
+    @todays_wday = Date.today#.wday
     @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
@@ -33,11 +33,13 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = @todays_wday# wdayメソッドを用いて取得した数値
-      if wday_num >= 7#「wday_numが7以上の場合」という条件式
-        return
-      end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: (@todays_wday+x).day}
+      #以下５行学習用に残してあります
+      # wday_num = Date.today.wday + x# wdayメソッドを用いて取得した数値
+      # if wday_num >= 7#「wday_numが7以上の場合」という条件式
+      #   return
+      # wday_num = wday_num -7
+      # end
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[(@todays_wday+x).wday]}
       @week_days.push(days)
     end
   end
